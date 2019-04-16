84c43a745b11b0ae550d536bc1fb58b90dbf624d
Brian Glowniak <bglowniak@gmail.com>
2019-04-16 00:33:46 -0400

    Add scatter plot and details on demand functionality

--- main.js
+++ main.js
@@ -1,124 +1,85 @@
-var margin = {top: 30, right: 40, bottom: 20, left: 200},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+width = 750;
+height = 450;
+chart_margins = {left: 50, right: 30, top: 30, bottom: 30};
 
-var dimensions, line, x, yAxis, svg;
+function appendDetailsBox(id, title, duration, year, rating, director, cast, country, budget, gross, score) {
+    $("#main").append("<div id=details-" + id + " class=details></div>")
+    id = "#details-" + id;
+    $(id).append("<p class=title>" + title + "</p>");
+    $(id).append("<p class=subheader>" + duration + " min | " + year + " | " + rating + "</p>");
+    $(id).append("<p><strong>Director: </strong>" + director + "</p>");
+    cast_members = cast.join(", ");
+    $(id).append("<p><strong>Cast: </strong>" + cast_members + "</p>");
+    $(id).append("<p><strong>Country: </strong>" + country + "</p>");
+    $(id).append("<p><strong>Budget: </strong>" + budget + "</p>");
+    $(id).append("<p><strong>Gross: </strong>" + gross + "</p>");
+    $(id).append("<p><strong>Score: </strong>" + score + "</p>");
+}
 
 d3.csv("movies.csv", (movies) => {
-
-    dimensions = [
-        {
-            name: "gross",
-            scale: d3.scaleLinear().range([height, 0]),
-        },
-        {
-            name: "budget",
-            scale: d3.scaleLinear().range([height, 0]),
-        },
-        {
-            name: "imdb score",
-            scale: d3.scaleLinear().range([height, 0]),
-        },
-        ];
-        
-        x = d3.scalePoint()
-            .domain(dimensions.map((d) => { return d.name; }))
-            .range([0, width]);
-        
-        line = d3.line();
-        
-        yAxis = d3.axisLeft();
-        
-        svg = d3.select('#main').append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-        
-        var dimension = svg.selectAll(".dimension")
-            .data(dimensions)
-        .enter().append("g")
-            .attr("class", "dimension")
-            .attr("transform", function(d) { return "translate(" + x(d.name) + ")"; });
-
-    for (let i = 0; i < movies.length; i++) {
+    for (var i = 0; i < movies.length; i++) {
         movies[i].id = i;
         movies[i].gross = +movies[i].gross;
         movies[i].budget = +movies[i].budget;
         movies[i].imdb_score = +movies[i].imdb_score;
     }
 
-    dimensions.forEach((dimension) => {
-        if (dimension.name == 'gross') {
-            dimension.scale.domain(d3.extent(movies, (d) => { return d.gross}));
-        } else if (dimension.name == 'budget') {
-            dimension.scale.domain(d3.extent(movies, (d) => { return d.budget}));
-        } else if (dimension.name == 'imdb_score') {
-            dimension.scale.domain(d3.extent(movies, (d) => { return d.imdb_score}));
-        }
-    });
+    var budgetExtent = d3.extent(movies, function(row) { return row.budget; });
+    var grossExtent = d3.extent(movies, function(row) { return row.gross; });
+    var imdbExtent = d3.extent(movies, function(row) { return row.imdb_score; });
 
-    svg.append("g")
-        .attr("class", "background")
-        .selectAll("path")
-        .data(movies)
-        .enter().append("path")
-        .attr("d", draw);
+    var xScale = d3.scalePow().exponent(0.4).domain(budgetExtent).range([chart_margins.left, width - chart_margins.right])
+    var yScale = d3.scalePow().exponent(0.4).domain(grossExtent).range([height - chart_margins.top, chart_margins.bottom])
+    var xAxis = d3.axisBottom().scale(xScale)
+    var yAxis = d3.axisLeft().scale(yScale)
 
-    svg.append("g")
-        .attr("class", "foreground")
-        .selectAll("path")
-        .data(movies)
-        .enter().append("path")
-        .attr("d", draw);
+    var plot = d3.select('#chart').append("svg")
+            .attr("width", width)
+            .attr("height", height);
 
-    dimension.append("g")
-        .attr("class", "axis")
-        .each(function(d) { d3.select(this).call(yAxis.scale(d.scale)); })
-        .append("text")
-        .attr("class", "title")
-        .attr("text-anchor", "middle")
-        .attr("y", -9)
-        .text(function(d) { return d.name; });
+    plot.selectAll("circle")
+       .data(movies)
+       .enter()
+       .append("circle")
+       .attr("id", function(d, i) {return i;} )
+       .attr("stroke", function(d) {
+            if (d.budget == 0 || d.gross == 0) {
+                return "red";
+            } else {
+                return "black";
+            }
+       })
+       .attr("cx", function(d) { return xScale(d.budget); })
+       .attr("cy", function(d) { return yScale(d.gross); })
+       .attr("r", 2)
+       .on("click", function(d, i) {
+            appendDetailsBox(i, d.movie_title,
+                                 d.duration,
+                                 d.title_year,
+                                 d.content_rating,
+                                 d.director_name,
+                                 [d.actor_1_name, d.actor_2_name, d.actor_3_name],
+                                 d.country,
+                                 d.budget,
+                                 d.gross,
+                                 d.imdb_score);
+       });
 
-    // var ordinal_labels = svg.selectAll(".axis text")
-    //     .on("mouseover", mouseover)
-    //     .on("mouseout", mouseout);
+    plot.append("g")
+        .call(xAxis.tickFormat(d => (d / 1000000) + "m"))
+        .attr("transform", "translate(0, " + (height - chart_margins.bottom) + ")")
+        .selectAll("text")
+        .attr("transform", function (d) { return "rotate(-30)"; });
 
-    // var projection = svg.selectAll(".background path,.foreground path")
-    //     .on("mouseover", mouseover)
-    //     .on("mouseout", mouseout);
 
-    // function mouseover(d) {
-    //     svg.classed("active", true);
+    plot.append("g")
+        .call(yAxis.tickFormat(d => (d / 1000000) + "m"))
+        .attr("transform", "translate(" + chart_margins.left + ", 0)")
 
-    //     // this could be more elegant
-    //     if (typeof d === "string") {
-    //     projection.classed("inactive", function(p) { return p.name !== d; });
-    //     projection.filter(function(p) { return p.name === d; }).each(moveToFront);
-    //     ordinal_labels.classed("inactive", function(p) { return p !== d; });
-    //     ordinal_labels.filter(function(p) { return p === d; }).each(moveToFront);
-    //     } else {
-    //     projection.classed("inactive", function(p) { return p !== d; });
-    //     projection.filter(function(p) { return p === d; }).each(moveToFront);
-    //     ordinal_labels.classed("inactive", function(p) { return p !== d.name; });
-    //     ordinal_labels.filter(function(p) { return p === d.name; }).each(moveToFront);
-    //     }
-    // }
 
-    // function mouseout(d) {
-    //     svg.classed("active", false);
-    //     projection.classed("inactive", false);
-    //     ordinal_labels.classed("inactive", false);
-    // }
-
-    // function moveToFront() {
-    //     this.parentNode.appendChild(this);
-    // }
-    });
-
-function draw(d) {
-    return line(dimensions.map(function(dimension) {
-        return [x(dimension.name), dimension.scale(d[dimension.name])];
-    }));
-}
+    /*plot.append('line')
+        .attr('x1', xScale(0))
+        .attr('x2', xScale(20))
+        .attr('y1', yScale(0))
+        .attr('y2', yScale(10))*/
+});
